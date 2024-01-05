daec8780d2902ab7c16238eb4b75286f9255e6ba
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2024-01-05 18:03:48 +0800

    modified tenant registration

--- components/TenantRegistration.vue
+++ components/TenantRegistration.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tableBody">
+  <div class="shadow p-3 mb-5 bg-white rounded" id="tableBody">
     <h1>Registration</h1>
     <div class="headerParam">
       <b-input-group prepend="Search" class="mt-3">
@@ -8,15 +8,22 @@
           @keyup.enter="fetchAllTenants()"
         ></b-form-input>
         <b-button
-          @click="show = true"
+          @click="onAddUser()"
           hover
           variant="outline-primary"
           class="ml-5"
-          >Add Tenant</b-button     
+          >Add Tenant</b-button
         >
       </b-input-group>
-
-      <b-modal v-model="show" title="Add Tenant">
+      <!-- modal -->
+      <b-modal
+        class="userModal"
+        id="userModal"
+        :title="userModalTitle"
+        no-close-on-backdrop
+        hide-footer
+        centered
+      >
         <b-form @submit="onSaveUser">
           <b-row class="mx-auto">
             <b-form-input
@@ -38,6 +45,20 @@
               debounce="500"
               placeholder="Last name"
             ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-select
+              id="select-role"
+              v-model="selected"
+              :options="optionRole"
+              required
+            >
+              <template #first>
+                <b-form-select-option :value="null" disabled
+                  >-- Gender --</b-form-select-option
+                >
+              </template>
+            </b-form-select>
           </b-row>
           <b-row class="mx-auto mt-2">
             <b-form-input
@@ -67,18 +88,16 @@
               placeholder="Region"
             ></b-form-input>
           </b-row>
-          <template #modal-footer>
-            <div class="w-100">
-              <b-button
-                variant="primary"
-                size="sm"
-                class="float-right"
-                @click="show = false"
-              >
-                Close
-              </b-button>
-            </div>
-          </template>
+          <hr class="mt-4" />
+          <div class="w-100">
+            <b-button variant="primary" class="float-right ml-1" type="submit">
+              <font-awesome-icon :icon="['fas', 'floppy-disk']" /> Save User
+              <!-- <b-spinner small v-show="loadingOnSave" /> -->
+            </b-button>
+            <b-button @click="$bvModal.hide('userModal')" class="float-right">
+              <font-awesome-icon :icon="['fas', 'xmark']" /> Cancel
+            </b-button>
+          </div>
         </b-form>
       </b-modal>
     </div>
@@ -93,11 +112,13 @@
       >
         <template #cell(action)="">
           <b-button size="sm">
-            <font-awesome-icon :icon="['fas', 'pen-to-square']" />
+            EDIT
+            <!-- <font-awesome-icon :icon="['fas', 'pen-to-square']" /> -->
           </b-button>
 
           <b-button size="sm">
-            <font-awesome-icon :icon="['fas', 'arrow-rotate-right']" />
+            DELETE
+            <!-- <font-awesome-icon :icon="['fas', 'arrow-rotate-right']" /> -->
           </b-button>
         </template>
       </b-table>
@@ -108,6 +129,7 @@
 </template>
   <script>
 import axios from "axios";
+import { log } from "console";
 export default {
   name: "TenantRegistration",
   data() {
@@ -116,17 +138,74 @@ export default {
       inputSearch: "",
       show: false,
       tenantList: [],
+      userModalTitle: "",
       tblHeaderCol: [
-        { key: "firstname", label: "First Name" },
-        { key: "middlename", label: "Middle Name" },
-        { key: "lastname", label: "Last Name" },
-        { key: "username", label: "Username" },
-        { key: "user_role", label: "Role" },
+        { key: "first_name", label: "First Name" },
+        { key: "middle_name", label: "Middle Name" },
+        { key: "last_name", label: "Last Name" },
+        { key: "city_address", label: "Address" },
+        { key: "gender", label: "Gender" },
         { key: "action", label: "Actions" },
+      ],
+      userInfo: {
+        first_name: "",
+        middle_name: "",
+        last_name: "",
+        address_line1: " ",
+        address_line2: "",
+        city_address: "",
+        provincial_address: "",
+        regional_address: "",
+        country: "",
+        gender: "",
+      },
+      selected: null,
+      optionRole: [
+        { value: "M", text: "Male" },
+        { value: "F", text: "Female" },
       ],
     };
   },
   methods: {
+    onAddUser() {
+      this.userInfo = {
+        first_name: "",
+        middle_name: "",
+        last_name: "",
+        address_line1: " ",
+        address_line2: "",
+        city_address: "",
+        provincial_address: "",
+        regional_address: "",
+        country: "",
+        gender: "",
+      };
+      this.userModalTitle = "Add Tenant";
+      this.$bvModal.show("userModal");
+    },
+    onSaveUser(e) {
+      e.preventDefault();
+
+      this.$bvModal
+        .msgBoxConfirm("Are you sure you want to Save user details?", {
+          title: "Please Confirm",
+          size: "sm",
+          buttonSize: "sm",
+          okVariant: "primary",
+          okTitle: "YES",
+          cancelTitle: "NO",
+          footerClass: "p-2",
+          centered: true,
+        })
+        .then((value) => {
+          if (value) this.doSaveUser();
+          this.loadingOnSave = false;
+        })
+        .catch((err) => {
+          this.loadingOnSave = false;
+          this.showAlert(err, "danger");
+        });
+    },
     async fetchAllTenants() {
       return await axios({
         method: "GET",
@@ -136,10 +215,9 @@ export default {
           this.tenantList = res.data.filter(
             function (val) {
               return (
-                val.firstname.includes(this.inputSearch) ||
-                val.middlename.includes(this.inputSearch) ||
-                val.username.includes(this.inputSearch) ||
-                val.lastname.includes(this.inputSearch)
+                val.first_name.includes(this.inputSearch) ||
+                val.middle_name.includes(this.inputSearch) ||
+                val.last_name.includes(this.inputSearch)
               );
             }.bind(this)
           );
@@ -152,11 +230,13 @@ export default {
   },
   created() {
     this.fetchAllTenants();
-  },     
+  },
+  onSaveUser(e) {},
+};
 </script>
 
 <style scoped>
-.tableBody {
+#tableBody {
   text-align: center;
   margin: 100px 50px 50px;
   /* border: 1px solid black; */

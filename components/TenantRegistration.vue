f84c9808f5085491d6d8d3b80aad861619b78609
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2024-01-05 08:57:26 +0800

    added table details

--- components/TenantRegistration.vue
+++ components/TenantRegistration.vue
@@ -3,7 +3,10 @@
     <h1>Registration</h1>
     <div class="headerParam">
       <b-input-group prepend="Search" class="mt-3">
-        <b-form-input></b-form-input>
+        <b-form-input
+          v-model="inputSearch"
+          @keyup.enter="fetchAllTenants()"
+        ></b-form-input>
         <b-button
           @click="show = true"
           hover
@@ -14,77 +17,92 @@
       </b-input-group>
 
       <b-modal v-model="show" title="Add Tenant">
-        <b-row class="mx-auto">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="First name"
-          ></b-form-input>
-        </b-row>
-        <b-row class="mx-auto mt-2">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="Middle name"
-          ></b-form-input>
-        </b-row>
-        <b-row class="mx-auto mt-2">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="Last name"
-          ></b-form-input>
-        </b-row>
-        <b-row class="mx-auto mt-2">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="Address line 1"
-          ></b-form-input>
-        </b-row>
-        <b-row class="mx-auto mt-2">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="Address line 2"
-          ></b-form-input>
-        </b-row>
-        <b-row class="mx-auto mt-2">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="Province"
-          ></b-form-input>
-        </b-row>
-        <b-row class="mx-auto mt-2">
-          <b-form-input
-            type="text"
-            debounce="500"
-            placeholder="Region"
-          ></b-form-input>
-        </b-row>
-        <template #modal-footer>
-          <div class="w-100">
-            <b-button
-              variant="primary"
-              size="sm"
-              class="float-right"
-              @click="show = false"
-            >
-              Close
-            </b-button>
-          </div>
-        </template>
+        <b-form @submit="onSaveUser">
+          <b-row class="mx-auto">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="First name"
+            ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="Middle name"
+            ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="Last name"
+            ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="Address line 1"
+            ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="Address line 2"
+            ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="Province"
+            ></b-form-input>
+          </b-row>
+          <b-row class="mx-auto mt-2">
+            <b-form-input
+              type="text"
+              debounce="500"
+              placeholder="Region"
+            ></b-form-input>
+          </b-row>
+          <template #modal-footer>
+            <div class="w-100">
+              <b-button
+                variant="primary"
+                size="sm"
+                class="float-right"
+                @click="show = false"
+              >
+                Close
+              </b-button>
+            </div>
+          </template>
+        </b-form>
       </b-modal>
     </div>
     <div class="dataBody">
+      <!-- data table start -->
       <b-table
         hover
         bordered
         :items="tenantList"
         :fields="tblHeaderCol"
         head-variant="light"
-      ></b-table>
+      >
+        <template #cell(action)="">
+          <b-button size="sm">
+            <font-awesome-icon :icon="['fas', 'pen-to-square']" />
+          </b-button>
+
+          <b-button size="sm">
+            <font-awesome-icon :icon="['fas', 'arrow-rotate-right']" />
+          </b-button>
+        </template>
+      </b-table>
+
+      <!-- data table end -->
     </div>
   </div>
 </template>
@@ -94,27 +112,18 @@ export default {
   name: "TenantRegistration",
   data() {
     return {
-      items: [
-        // {
-        //   age: 40,
-        //   first_name: "Dickerson",
-        //   last_name: "Macdonald",
-        //   course: "BSCpE",
-        // },
-        // { age: 21, first_name: "Larsen", last_name: "Shaw", course: "BSCpE" },
-        // { age: 89, first_name: "Geneva", last_name: "Wilson", course: "BSCpE" },
-        // { age: 38, first_name: "Jami", last_name: "Carney", course: "BSCpE" },
-      ],
+      items: [],
+      inputSearch: "",
       show: false,
+      tenantList: [],
       tblHeaderCol: [
         { key: "firstname", label: "First Name" },
         { key: "middlename", label: "Middle Name" },
         { key: "lastname", label: "Last Name" },
         { key: "username", label: "Username" },
         { key: "user_role", label: "Role" },
+        { key: "action", label: "Actions" },
       ],
-
-      tenantList: [],
     };
   },
   methods: {
@@ -122,28 +131,28 @@ export default {
       return await axios({
         method: "GET",
         url: `${this.$axios.defaults.baseURL}/user/getAllUser`,
-      }).then((res) => {
-        this.tenantList = res.data.filter(
-          function (val) {
-            return (
-              (val.firstname.includes(this.inputSearch) ||
+      }).then(
+        (res) => {
+          this.tenantList = res.data.filter(
+            function (val) {
+              return (
+                val.firstname.includes(this.inputSearch) ||
                 val.middlename.includes(this.inputSearch) ||
                 val.username.includes(this.inputSearch) ||
-                val.lastname.includes(this.inputSearch)) &&
-              val.user_role.includes(this.selectedRole)
-            );
-          }.bind(this)
-        );
-      }, (err) => {
-        console.log("ERROR:",err);
-      });
-
+                val.lastname.includes(this.inputSearch)
+              );
+            }.bind(this)
+          );
+        },
+        (err) => {
+          console.log("ERROR:", err);
+        }
+      );
     },
   },
-  created(){
+  created() {
     this.fetchAllTenants();
-    console.log("tenantList:",tenantList)
-  }
+  },
 };
 </script>
 
